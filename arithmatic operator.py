818419b6fe87b52e60792f1086add5919dac6c85
sanjuktassc <81350704+sanjuktassc@users.noreply.github.com>
2021-03-26 10:22:50 +0530

    Add files via upload

---
+++ arithmatic operator.py
@@ -0,0 +1,27 @@
+print("1.ADDITION");
+print("2.SUBTRACTION");
+print("3.MULTIPLICATION");
+print("4.DIVISION");
+print("5.EXIT");
+choice=int(input("enter your choice:"));
+if(choice>=1 and choice<=4):
+     print("enter two numbers:");
+num1=int(input());
+num2=int(input());
+if choice==1:
+     res=num1+num2;
+     print("Result =", res);
+elif choice==2:
+      res=num1-num2;
+      print("result=",res);
+elif choice==3: 
+      res = num1 * num2;
+      print("Result=", res);
+elif choice==4:
+      res=num1/num2;
+      print("Result=", res);
+elif choice== 5:
+     exit();
+    
+else:
+    print("Wrong input..!!"); 
